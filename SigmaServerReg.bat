1efa6594e83bc535e63a9e8def72266963a90efa
tkrome <32278922+tkrome@users.noreply.github.com>
2018-04-02 13:34:52 -0400

    Initial commit

---
+++ SigmaServerReg.bat
@@ -0,0 +1,13 @@
+@echo off
+title SigmaStudio COM Server Registration
+set /p SSVER="Enter SigmaStudio version (i.e. 3.17): 3."
+set /p MVER="Enter MATLAB bit version (32 or 64): "
+set OLDPATH=%CD%
+
+if %MVER%==32 if %SSVER% leq 16 cd C:\Windows\Microsoft.NET\Framework\v2.0.50727 && RegAsm "C:\Program Files\Analog Devices\SigmaStudio 3.%SSVER%\Analog.SigmaStudioServer.dll" /codebase
+if %MVER%==64 if %SSVER% leq 16 cd C:\Windows\Microsoft.NET\Framework64\v2.0.50727 && RegAsm "C:\Program Files\Analog Devices\SigmaStudio 3.%SSVER%\Analog.SigmaStudioServer.dll" /codebase
+
+if %MVER%==32 if %SSVER% geq 17 cd C:\Windows\Microsoft.NET\Framework\v4.0.30319 && RegAsm "C:\Program Files\Analog Devices\SigmaStudio 3.%SSVER%\Analog.SigmaStudioServer.dll" /codebase
+if %MVER%==64 if %SSVER% geq 17 cd C:\Windows\Microsoft.NET\Framework64\v4.0.30319 && RegAsm "C:\Program Files\Analog Devices\SigmaStudio 3.%SSVER%\Analog.SigmaStudioServer.dll" /codebase
+pause
+cd %OLDPATH%
